e3b0fd11cb0b407ddd80d3bad4b196401101951e
Simone Simões <simone.simoes991@gmail.com>
2021-04-28 12:03:09 -0300

    Create aula04.py

---
+++ aula04.py
@@ -0,0 +1,34 @@
+aluno1 = float(input('Digite a nota do aluno 1: '))
+aluno2 = float(input('Digite a nota do aluno 2: '))
+aluno3 = float(input('Digite a nota do aluno 3: '))
+aluno4 = float(input('Digite a nota do aluno 4: '))
+aluno5 = float(input('Digite a nota do aluno 5: '))
+aluno6 = float(input('Digite a nota do aluno 6: '))
+aluno7 = float(input('Digite a nota do aluno 7: '))
+aluno8 = float(input('Digite a nota do aluno 8: '))
+aluno9 = float(input('Digite a nota do aluno 9: '))
+aluno10 = float(input('Digite a nota do aluno 10: '))
+
+# ...
+
+```
+Nota do programador: eu me demito. 
+Não ganho bem o suficiente pra ISSO!
+```
+
+primeiralista = [] # cria uma lista vazia
+segundalista = list() # cria uma lista vazia
+terceiralista = [1, 3.14, 5, 7, 9, 'onze'] # lista com valores
+
+
+terceiralista = [1, 3.14, 5, 7, 9, 'onze'] # lista com valores
+terceiralista[3] = 'sete' # troca 7 por 'sete' na lista
+print(terceiralista)
+
+impares = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+primeirametade = impares[:5]
+segundametade = impares[5:]
+print(primeirametade) # resultado: [1, 3, 5, 7, 9]
+print(segundametade) # resultado: [11, 13, 15, 17, 19]
+
+nota1 = float(input('Digite sua idade'))
