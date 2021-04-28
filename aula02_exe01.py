229f3bf2d81eda30ec5f0aa119e45b39a1d44edf
Simone Simões <simone.simoes991@gmail.com>
2021-04-28 11:54:11 -0300

    Create aula02_exe01.py

---
+++ aula02_exe01.py
@@ -0,0 +1,11 @@
+print('   / |')
+print('  /  |')
+print(' /   |')
+print('/____|')
+
+
+
+media = float(input("Digite a média do aluno:"))
+presença = float(input("Digite as presenças do aluno:"))
+aprovado = media >=6.0 and presença >=0.75
+print(aprovado)
