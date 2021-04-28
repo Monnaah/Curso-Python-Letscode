e5a0f3406c2caeac37b6f751c8cdbaf4716bb053
Simone Simões <simone.simoes991@gmail.com>
2021-04-28 12:06:06 -0300

    Create aula05.py

---
+++ aula05.py
@@ -0,0 +1,40 @@
+'''
+Crie uma função eh_par que recebe um número e retorna 1 se esse número for par e 0 se o número for ímpar.
+
+Exemplo:
+>>> eh_par(2)
+>>> 1
+>>> eh_par(3)
+>>> 0
+'''
+def eh_par (numero):
+    if numero % 2 == 0:
+        return 1
+    else:
+        return 0
+numero = 6
+print(eh_par(numero))
+
+
+'''
+Reutilizando a função eh_par criada no exercício anterior, crie uma nova função, 
+conta_pares que recebe uma lista com números e retorna a quantidade de números pares.
+
+Exemplo:
+
+>>> conta_pares([1,2,3])
+>>> 1
+>>> conta_pares([2,4,6])
+>>> 3
+'''
+def eh_par (numero):
+    if numero % 2 == 0:
+        return 1
+    else:
+        return 0
+numero = 6
+
+def conta_pares (n):
+    nu = 1,2,3,4,5,6
+    for pares in range 
+        print(n)
